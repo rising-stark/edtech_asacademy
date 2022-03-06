a6a29eec85beca27948ffc0a91b3705e1cc0f801
haskh <hasan78612021@Gmail.com>
2022-03-06 19:02:14 +0000

    made all pages mobile responsive, please test thorougly.

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -55,3 +55,37 @@ $(window).scroll(function(){
       $('.navbar').removeClass('sticky')
   }
 });
+
+
+
+function mobileMenu() {
+  var x = document.getElementById("menu");
+  if (x.className === "mobile-menu") {
+      x.className += " responsive";
+  } else {
+      x.className = "mobile-menu";
+  }
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("banner");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
