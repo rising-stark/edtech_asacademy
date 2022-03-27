652965774f21e74cf39299c34738c6c696e6d6e7
0xcrypto <6683856+0xcrypto@users.noreply.github.com>
2022-03-27 19:37:43 +0530

    added mysql support for heroku

--- asacademy/settings.py
+++ asacademy/settings.py
@@ -18,7 +18,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
